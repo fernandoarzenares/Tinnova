7b04e0f423fa5ca85ea9f24e373a9ec6234070a4
Fernando César Scialo Arzenares <106258238+fernandoarzenares@users.noreply.github.com>
2022-05-27 17:21:22 -0300

    Delete ex004_a.py

--- Exercício_04/ex004_a.py
+++
@@ -1,9 +0,0 @@
-# Soma dos múltiplos de 3 ou 5
-# Somar todos os números múltiplos de 3 ou 5 que estejam entre 0 até o número digitado
-
-n = int(input('Digite um número: '))
-soma = 0
-for c in range(0, n+1):
-    if c % 3 == 0 or c % 5 == 0:
-        soma += c
-print(f'A soma dos múltiplos de 3 e 5 entre 0 e {n} é {soma}')
